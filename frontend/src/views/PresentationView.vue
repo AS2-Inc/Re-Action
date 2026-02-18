98c53c9ad4e6a65cd7e007806c0d70ac0318b5e4
Simone Tardiani <simone.tardiani.imp@gmail.com>
2026-02-18 18:17:02 +0100

    refactor: fromatted and linted

--- frontend/src/views/PresentationView.vue
+++ frontend/src/views/PresentationView.vue
@@ -19,6 +19,7 @@
 <script>
 import Button from "@/components/Button.vue";
 import Statistic from "../components/Statistic.vue";
+
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 export default {
